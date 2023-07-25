ae011cd4b73202e271379cb8ef4b7923658d4cfb
Sergei Jeihala <sergejey@gmail.com>
2023-07-25 21:38:18 +0300

    Update zigbeedevices_edit.inc.php

--- modules/zigbeedev/zigbeedevices_edit.inc.php
+++ modules/zigbeedev/zigbeedevices_edit.inc.php
@@ -41,7 +41,7 @@ if ($this->mode == 'update') {
 }
 
 if (gr('ok')) {
-    $out['OK']=1;
+    $out['OK'] = 1;
 }
 
 // step: default
@@ -57,7 +57,7 @@ if ($this->tab == 'data') {
     }
     $properties = SQLSelect("SELECT * FROM zigbeeproperties WHERE DEVICE_ID='" . $rec['ID'] . "' ORDER BY TITLE");
     $total = count($properties);
-    $prop_id=gr('prop_id','int');
+    $prop_id = gr('prop_id', 'int');
     for ($i = 0; $i < $total; $i++) {
         if ($properties[$i]['ID'] == $new_id) continue;
         if ($properties[$i]['ID'] == $prop_id) {
@@ -66,53 +66,26 @@ if ($this->tab == 'data') {
                 $old_linked_object = $properties[$i]['LINKED_OBJECT'];
                 $old_linked_property = $properties[$i]['LINKED_PROPERTY'];
 
-                $properties[$i]['LINKED_OBJECT']=gr('linked_object','trim');
-                $properties[$i]['LINKED_PROPERTY']=gr('linked_property','trim');
-                $properties[$i]['LINKED_METHOD']=gr('linked_method','trim');
-                $properties[$i]['READ_ONLY']=gr('read_only','trim');
-                $properties[$i]['PROCESS_TYPE']=gr('process_type','int');
-                SQLUpdate('zigbeeproperties',$properties[$i]);
+                $properties[$i]['LINKED_OBJECT'] = gr('linked_object', 'trim');
+                $properties[$i]['LINKED_PROPERTY'] = gr('linked_property', 'trim');
+                $properties[$i]['LINKED_METHOD'] = gr('linked_method', 'trim');
+                $properties[$i]['READ_ONLY'] = gr('read_only', 'trim');
+                $properties[$i]['PROCESS_TYPE'] = gr('process_type', 'int');
+                SQLUpdate('zigbeeproperties', $properties[$i]);
 
-                if (($old_linked_object && $old_linked_object != $properties[$i]['LINKED_OBJECT']) ||
-                    ($old_linked_property && $old_linked_property != $properties[$i]['LINKED_PROPERTY'])) {
-                    removeLinkedProperty($old_linked_object, $old_linked_property, $this->name);
-                }
                 if ($properties[$i]['LINKED_OBJECT'] && $properties[$i]['LINKED_PROPERTY']) {
                     addLinkedProperty($properties[$i]['LINKED_OBJECT'], $properties[$i]['LINKED_PROPERTY'], $this->name);
+                } elseif ($old_linked_object && $old_linked_property && function_exists('removeLinkedPropertyIfNotUsed')) {
+                    removeLinkedPropertyIfNotUsed('zigbeeproperties', $old_linked_object, $old_linked_property, $this->name);
                 }
+                $this->redirect("?id=" . $rec['ID'] . "&view_mode=" . $this->view_mode . "&tab=" . $this->tab . "&prop_id=" . $prop_id . "&ok=1");
+            }
+            foreach ($properties[$i] as $k => $v) {
+                $out['PROP_' . $k] = $v;
+            }
+        }
+        $properties[$i]['VALUE'] = str_replace('",', '", ', $properties[$i]['VALUE']);
 
-                $this->redirect("?id=".$rec['ID']."&view_mode=".$this->view_mode."&tab=".$this->tab."&prop_id=".$prop_id."&ok=1");
-            }
-            foreach($properties[$i] as $k=>$v) {
-                $out['PROP_'.$k]=$v;
-            }
-        }
-        $properties[$i]['VALUE']=str_replace('",','", ',$properties[$i]['VALUE']);
-        if ($this->mode == 'update') {
-            /*
-            global ${'title' . $properties[$i]['ID']};
-            $properties[$i]['TITLE'] = trim(${'title' . $properties[$i]['ID']});
-            global ${'value' . $properties[$i]['ID']};
-            $properties[$i]['VALUE'] = trim(${'value' . $properties[$i]['ID']});
-            global ${'linked_object' . $properties[$i]['ID']};
-            $properties[$i]['LINKED_OBJECT'] = trim(${'linked_object' . $properties[$i]['ID']});
-            global ${'linked_property' . $properties[$i]['ID']};
-            $properties[$i]['LINKED_PROPERTY'] = trim(${'linked_property' . $properties[$i]['ID']});
-            global ${'linked_method' . $properties[$i]['ID']};
-            $properties[$i]['LINKED_METHOD'] = trim(${'linked_method' . $properties[$i]['ID']});
-            global ${'read_only' . $properties[$i]['ID']};
-            $properties[$i]['READ_ONLY'] = trim(${'read_only' . $properties[$i]['ID']});
-            SQLUpdate('zigbeeproperties', $properties[$i]);
-            $old_linked_object = $properties[$i]['LINKED_OBJECT'];
-            $old_linked_property = $properties[$i]['LINKED_PROPERTY'];
-            if ($old_linked_object && $old_linked_object != $properties[$i]['LINKED_OBJECT'] && $old_linked_property && $old_linked_property != $properties[$i]['LINKED_PROPERTY']) {
-                removeLinkedProperty($old_linked_object, $old_linked_property, $this->name);
-            }
-            if ($properties[$i]['LINKED_OBJECT'] && $properties[$i]['LINKED_PROPERTY']) {
-                addLinkedProperty($properties[$i]['LINKED_OBJECT'], $properties[$i]['LINKED_PROPERTY'], $this->name);
-            }
-            */
-        }
     }
     $out['PROPERTIES'] = $properties;
     if (gr('ajax')) {
